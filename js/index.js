35e9ce4c293d630a46b783f4675c33ae4c5b1693
unknown <nguyenvanty10987654321000@gmail.com>
2021-01-23 00:49:28 +0700

    update 26

--- js/index.js
+++ js/index.js
@@ -43,7 +43,18 @@ if (popup) {
     }, 500);
   });
 }
-
+// login
+const login = document.querySelector(".login");
+const popup__close_login = document.querySelector(".popup__close_login");
+const login_parent = document.querySelector(".login_parent");
+if (login_parent) {
+  login.addEventListener("click", (e) => {
+    login_parent.classList.add("show");
+  });
+  popup__close_login.addEventListener("click", (e) => {
+    login_parent.classList.remove("show");
+  });
+}
 /*
 =============
 Fixed Navigation
@@ -101,42 +112,42 @@ window.addEventListener("scroll", e => {
 // animation class="animate-left"
 window.sr = ScrollReveal();
 sr.reveal('.animate-left', {
-    origin: 'left',
-    duration: 1000,
-    distance: '25rem',
-    delay: 300
+  origin: 'left',
+  duration: 1000,
+  distance: '25rem',
+  delay: 300
 });
 sr.reveal('.animate-right', {
-    origin: 'right',
-    duration: 1000,
-    distance: '25rem',
-    delay: 300
+  origin: 'right',
+  duration: 1000,
+  distance: '25rem',
+  delay: 300
 });
 sr.reveal('.animate-top', {
-    origin: 'top',
-    duration: 1000,
-    distance: '25rem',
-    delay: 300
+  origin: 'top',
+  duration: 1000,
+  distance: '25rem',
+  delay: 300
 });
 sr.reveal('.animate-bottom', {
-    origin: 'bottom',
-    duration: 1000,
-    distance: '25rem',
-    delay: 300
+  origin: 'bottom',
+  duration: 1000,
+  distance: '25rem',
+  delay: 300
 });
 // insert 4 + 5
 const responsive = {
   0: {
-      items: 1
+    items: 1
   },
   320: {
-      items: 1
+    items: 1
   },
   560: {
-      items: 2
+    items: 2
   },
   960: {
-      items: 3
+    items: 3
   }
 }
 $(document).ready(function () {
@@ -151,21 +162,21 @@ $(document).ready(function () {
 
   // owl-crousel for blog
   $('.owl-carousel').owlCarousel({
-      loop: true,
-      autoplay: false,
-      autoplayTimeout: 3000,
-      dots: false,
-      nav: true,
-      navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-      responsive: responsive
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive: responsive
   });
 
 
   // click to scroll top
   $('.move-up span').click(function () {
-      $('html, body').animate({
-          scrollTop: 0
-      }, 1000);
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000);
   })
 
   // AOS Instance
@@ -177,52 +188,52 @@ $(document).ready(function () {
   "use strict"
   // hiệu ứng chuyển động nước
   $('.slider').ripples({
-      dropRadius: 13,
-      perturbance: 0.01,
+    dropRadius: 13,
+    perturbance: 0.01,
   });
 
   // hiệu ứng chuyển động chữ
   var typed = new Typed('.typed', {
-      stringsElement: '#typed-strings',
-      //     strings: ['i love<strong class="primary"> codings.</strong>', 'and to<strong class="primary"> share !!.</strong>'],
-      typespeed: 0,
-      // lặp lại vô hạn
-      loop: true
+    stringsElement: '#typed-strings',
+    //     strings: ['i love<strong class="primary"> codings.</strong>', 'and to<strong class="primary"> share !!.</strong>'],
+    typespeed: 0,
+    // lặp lại vô hạn
+    loop: true
   });
 
   const gotoTop = document.querySelector(".container1");
   $(window).scroll(function () {
-      var top = $(window).scrollTop();
-      if (top >= 10) {
-          gotoTop.classList.add("secondary");
-          // $("nav").addClass('secondary');
-      }
-      else {
-          gotoTop.classList.remove("secondary");
-          //   $("nav").removeClass('secondary');
-      }
+    var top = $(window).scrollTop();
+    if (top >= 10) {
+      gotoTop.classList.add("secondary");
+      // $("nav").addClass('secondary');
+    }
+    else {
+      gotoTop.classList.remove("secondary");
+      //   $("nav").removeClass('secondary');
+    }
   });
 
 
 
   // hiệu ứng hiển thị và load khi click vào ảnh
   $('.work').magnificPopup({
-      delegate: 'a',
-      type: 'image',
-      gallery: {
-          enabled: true
-      }
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
   });
 
   // slider
   $('.bxslider').bxSlider({
-      mode: 'horizontal',
-      moveSlides: 1,
-      slideMargin: 40,
-      infiniteLoop: true,
-      minSliders: 1,
-      maxSlides: 1,
-      speed: 1200,
+    mode: 'horizontal',
+    moveSlides: 1,
+    slideMargin: 40,
+    infiniteLoop: true,
+    minSliders: 1,
+    maxSlides: 1,
+    speed: 1200,
 
   });
 
@@ -230,44 +241,44 @@ $(document).ready(function () {
 
   // ảnh swiper
   if ($(".swiper-container").hasClass("team-member-slider")) {
-      var swiper = new Swiper('.swiper-container', {
-          slidesPerView: 3,
-          allowTouchMove: true,
-          loop: true,
-          centeredSlides: true,
-          slideToclickedslide: true,
-          effect: "coverflow",
-          grabcursor: true,
-          autoplay: false,
-          navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev'
-          },
-          coverflow: {
-              rotate: 0,
-              stretch: 100,
-              depth: 200,
-              modifier: 1,
-              slideShadows: false
-          },
-          breakpoints: {
-              767: {
-                  slidesPerView: 1,
-                  centeredSlides: false,
-                  effect: "slide",
-              }
-          }
-      });
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      allowTouchMove: true,
+      loop: true,
+      centeredSlides: true,
+      slideToclickedslide: true,
+      effect: "coverflow",
+      grabcursor: true,
+      autoplay: false,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      coverflow: {
+        rotate: 0,
+        stretch: 100,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false
+      },
+      breakpoints: {
+        767: {
+          slidesPerView: 1,
+          centeredSlides: false,
+          effect: "slide",
+        }
+      }
+    });
   }
 
 
 
   $("#work").magnificPopup({
-      delegate: 'a',
-      type: 'image',
-      gallery: {
-          enabled: true
-      }
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
   });
 
 
@@ -275,15 +286,15 @@ $(document).ready(function () {
 
   const questions = document.querySelectorAll(".question");
   questions.forEach(function (question) {
-      const btn = question.querySelector(".question-title");
-      btn.addEventListener("click", function () {
-          // questions.forEach(function (item) {
-          //     if (item !== question) {
-          //         item.classList.remove("show-text");
-          //     }
-          // });
-          question.classList.toggle("show-text");
-      });
+    const btn = question.querySelector(".question-title");
+    btn.addEventListener("click", function () {
+      // questions.forEach(function (item) {
+      //     if (item !== question) {
+      //         item.classList.remove("show-text");
+      //     }
+      // });
+      question.classList.toggle("show-text");
+    });
   });
 
 
@@ -295,14 +306,14 @@ $(document).ready(function () {
   let tabsPane = tabHeader.getElementsByTagName("div");
 
   for (let i = 0; i < tabsPane.length; i++) {
-      tabsPane[i].addEventListener("click", function () {
-          tabHeader.querySelector(".active").classList.remove("active");
-          // khi click vào thằng div thứ 2 trong header
-          tabsPane[i].classList.add("active");
-          tabBody.querySelector(".active").classList.remove("active");
-          tabBody.getElementsByTagName("div")[i].classList.add("active");
-          tabIndicator.style.left = `calc(calc(100% / 4) * ${i})`;
-      });
+    tabsPane[i].addEventListener("click", function () {
+      tabHeader.querySelector(".active").classList.remove("active");
+      // khi click vào thằng div thứ 2 trong header
+      tabsPane[i].classList.add("active");
+      tabBody.querySelector(".active").classList.remove("active");
+      tabBody.getElementsByTagName("div")[i].classList.add("active");
+      tabIndicator.style.left = `calc(calc(100% / 4) * ${i})`;
+    });
   }
 
 
@@ -310,39 +321,39 @@ $(document).ready(function () {
 
 
   $(document).ready(function () {
-      $('.swift').click(function () {
-          var signButton = $(this).html();
-          console.log("test 1: " + signButton);
-          debugger;
-          if (signButton == 'Sign In') {
-              $('.sign_up').html('Sign In'); // sign up button text change
-              $('.sign_in').html('Sign Up'); // sign in button text change
-              $('.swift_right').show(); // second logo show
-              $('.b-forgot').show(); // forgot option show
-              $('.form_title').html('Sign in to Guide'); // form title text change
-              $('.b-subtext').html('or use your email account'); // form sub title text change
-              $('.user_title').html('Hello, Friend'); // user title text change
-              $('.user_subTitle').html('Enter your personal details </br> and start journey with us.'); // user sub title text change
-             // $('.b-title').css('margin-top', '100px'); // user section add margin top in css
-              $('.swift_left').hide(); // default logo hide
-              $('.username').hide(); // form user field hide
-              $('.b-wrapper').addClass('swift_element'); // add reverse
-          } else {
-              $('.sign_up').html('Sign Up'); // sign up button text change
-              $('.sign_in').html('Sign In'); // sign in button text change
-              $('.swift_right').hide(); // second logo hide
-              $('.b-forgot').hide(); // forgot option hide
-              $('.form_title').html('Create Account');
-              $('.b-subtext').html('or use your email for registration');
-              $('.user_title').html('Welcome Friend');
-              $('.user_subTitle').html('To keep Connected with us please </br> login with your personal info.');
-              $('.b-title').css('margin-top', '0px');
-              $('.swift_left').show();
-              $('.username').show();
-              $('.b-wrapper').removeClass('swift_element');
-          }
+    $('.swift').click(function () {
+      var signButton = $(this).html();
+      console.log("test 1: " + signButton);
+      debugger;
+      if (signButton == 'Sign In') {
+        $('.sign_up').html('Sign In'); // sign up button text change
+        $('.sign_in').html('Sign Up'); // sign in button text change
+        $('.swift_right').show(); // second logo show
+        $('.b-forgot').show(); // forgot option show
+        $('.form_title').html('Sign in to Guide'); // form title text change
+        $('.b-subtext').html('or use your email account'); // form sub title text change
+        $('.user_title').html('Hello, Friend'); // user title text change
+        $('.user_subTitle').html('Enter your personal details </br> and start journey with us.'); // user sub title text change
+        // $('.b-title').css('margin-top', '100px'); // user section add margin top in css
+        $('.swift_left').hide(); // default logo hide
+        $('.username').hide(); // form user field hide
+        $('.b-wrapper').addClass('swift_element'); // add reverse
+      } else {
+        $('.sign_up').html('Sign Up'); // sign up button text change
+        $('.sign_in').html('Sign In'); // sign in button text change
+        $('.swift_right').hide(); // second logo hide
+        $('.b-forgot').hide(); // forgot option hide
+        $('.form_title').html('Create Account');
+        $('.b-subtext').html('or use your email for registration');
+        $('.user_title').html('Welcome Friend');
+        $('.user_subTitle').html('To keep Connected with us please </br> login with your personal info.');
+        $('.b-title').css('margin-top', '0px');
+        $('.swift_left').show();
+        $('.username').show();
+        $('.b-wrapper').removeClass('swift_element');
+      }
 
-      })
+    })
   })
 
 });
